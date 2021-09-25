bbc4d3a76aecbb3168520586e53d0298c8c289e1
Slysux <cajita2002@outlook.es>
2021-09-24 20:18:55 -0500

    feat: Styles finished. fix: Character.js classes changed

--- src/pages/Character.js
+++ src/pages/Character.js
@@ -6,17 +6,23 @@ const Character = async () => {
     const character = await getData(id);
     const view = `
         <div class="characters-inner">
-            <article class="characters-item">
+            <article class="characters-inner__item">
                 <img src="${character.image}" alt="${character.name}">
                 <h2>${character.name}</h2>
             </article>
-            <article class="characters-card">
-                <h3>Espisodes: ${character.episode.length}</h3>
-                <h3>Status: <span>${character.status}</span></h3>
-                <h3>Species: <span>${character.species}</span></h3>
-                <h3>Gender: <span>${character.gender}</span></h3>
-                <h3>Origin: <span>${character.origin.name}</span></h3>
-                <h3>Last location: <span>${character.location.name}</span></h3>
+            <article class="characters-inner__card">
+                <h3>Episodes: </h3>
+                <span>${character.episode.length}</span>
+                <h3>Status: </h3>
+                <span>${character.status}</span>
+                <h3>Species: </h3>
+                <span>${character.species}</span>
+                <h3>Gender: </h3>
+                <span>${character.gender}</span>
+                <h3>Origin: </h3>
+                <span>${character.origin.name}</span>
+                <h3>Last location: </h3>
+                <span>${character.location.name}</span>
             </article>
         </div>   
     `;
